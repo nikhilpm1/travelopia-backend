160a3001194bb6ebdd66cb04fbb375156c966be6
nikhilpm1 <nikhil@hamon.in>
2022-12-12 20:43:40 +0530

    assining file to a variable to get all functions

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
-
 const port = 3001
+const db = require('./queries')
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 
 
