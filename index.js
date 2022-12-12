acaf6cc5382841403166a8eaccaac060edd98925
nikhilpm1 <nikhil@hamon.in>
2022-12-12 20:48:53 +0530

    defining cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
+const cors = require('cors')
 const port = 3001
 const db = require('./queries')
 
