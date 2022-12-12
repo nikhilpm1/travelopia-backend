7b98ffb79c92aa4b8746044e761e7425858768ff
nikhilpm1 <nikhil@hamon.in>
2022-12-12 20:38:46 +0530

    creating endpoint to get all bookings

--- queries.js
+++ queries.js
@@ -7,3 +7,12 @@ const pool = new Pool({
   port: 5432,
 })
 
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
