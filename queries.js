9c8212903b9e27e9f23716878733e8330002394a
nikhilpm1 <nikhil@hamon.in>
2022-12-12 20:40:10 +0530

    creating another endpoint to create new bookings

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   port: 5432,
 })
 
-const getUsers = (request, response) => {
+const getAllBookings = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
       if (error) {
         throw error
@@ -16,3 +16,15 @@ const getUsers = (request, response) => {
     })
   }
 
+  const createBooking = (request, response) => {
+    const { first_name,last_name, email, country, no_of_persons, budget } = request.body
+  
+    pool.query('INSERT INTO users (first_name,last_name, email, country, no_of_persons, budget) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [first_name,last_name, email, country, no_of_persons, budget], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`User created with ID: ${results.rows[0].id}`)
+    })
+  }
+
+  
